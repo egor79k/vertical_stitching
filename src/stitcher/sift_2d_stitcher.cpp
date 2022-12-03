6bde9813d36d57f8c1b4f321118af6a6b5da8067
egor <Egor Kiselev>
2022-12-03 11:36:19 +0300

    DoG calculation added

--- src/stitcher/sift_2d_stitcher.cpp
+++ src/stitcher/sift_2d_stitcher.cpp
@@ -13,24 +13,17 @@ std::shared_ptr<VoxelContainer> SIFT2DStitcher::stitch(const VoxelContainer& sca
 
 
 void SIFT2DStitcher::detect(cv::Mat img) {
-    // cv::namedWindow("Display Keypoints", cv::WINDOW_AUTOSIZE);
-    // cv::normalize(img, img, 0, 1, cv::NORM_MINMAX);
-    // cv::imshow("Display Keypoints", img);
-    // cv::waitKey(0);
-    // cv::GaussianBlur(img, img, cv::Size(5, 5), 0, 0);
-    // cv::imshow("Display Keypoints", img);
-    // cv::waitKey(0);
-    // cv::destroyAllWindows();
-
     const int octaves_num = 4;
     const double scale_levels_num = 5;
     double sigma = 1.6;
     const double k = std::pow(std::sqrt(2), 1 / scale_levels_num);
     
     std::vector<std::vector<cv::Mat>> gaussians(octaves_num);
+    std::vector<std::vector<cv::Mat>> DoG(octaves_num);
 
     for (int i = 0; i < octaves_num; ++i) {
         gaussians[i].resize(scale_levels_num);
+        DoG[i].resize(scale_levels_num - 1);
     }
 
     cv::namedWindow("Display Keypoints", cv::WINDOW_AUTOSIZE);
@@ -40,12 +33,39 @@ void SIFT2DStitcher::detect(cv::Mat img) {
             printf("%s %i %s %i\n", "Octave:", octave, "Scale level:", scale_level);
             sigma *= k;
             cv::GaussianBlur(img, gaussians[octave][scale_level], cv::Size(5, 5), sigma);
-            cv::normalize(gaussians[octave][scale_level], gaussians[octave][scale_level], 0, 1, cv::NORM_MINMAX);
-            cv::imshow("Display Keypoints", gaussians[octave][scale_level]);
+            // cv::normalize(gaussians[octave][scale_level], gaussians[octave][scale_level], 0, 1, cv::NORM_MINMAX);
+            // cv::imshow("Display Keypoints", gaussians[octave][scale_level]);
+            // int key = -1;
+            // while (key != 'q') key = cv::waitKeyEx(100);
+        }
+
+        cv::resize(img, img, cv::Size(), 0.5, 0.5);
+    }
+
+    // cv::destroyAllWindows();
+
+    for (int octave = 0; octave < octaves_num; ++ octave) {
+        for (int scale_level = 0; scale_level < scale_levels_num - 1; ++scale_level) {
+            DoG[octave][scale_level] = gaussians[octave][scale_level] - gaussians[octave][scale_level + 1];
+            cv::normalize(DoG[octave][scale_level], DoG[octave][scale_level], 0, 1, cv::NORM_MINMAX);
+            cv::imshow("Display Keypoints", DoG[octave][scale_level]);
             int key = -1;
             while (key != 'q') key = cv::waitKeyEx(100);
         }
     }
 
     cv::destroyAllWindows();
+
+    // for (int octave = 0; octave < octaves_num; ++ octave) {
+    //     for (int scale_level = 1; scale_level < scale_levels_num - 2; ++scale_level) {
+    //         for (int y = 1; y < DoG[octave][scale_level].rows - 1; ++y) {
+    //             for (int x = 1; x < DoG[octave][scale_level].cols - 1; ++x) {
+    //                 float candidate = DoG[octave][scale_level].at(y, x);
+    //                 if (candidate > DoG[octave][scale_level - 1].at(y, x)) {
+    //                     printf("%i %i", y, x);
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
